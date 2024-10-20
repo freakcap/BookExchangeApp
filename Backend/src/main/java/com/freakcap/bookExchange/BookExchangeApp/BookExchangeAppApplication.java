f32b506a4fc9f3984a8e9f5e2e7fe0f27bacc7c3
Pranav Kulkarni <pranavkulkarni@Pranavs-MacBook-Air.local>
2024-10-20 16:03:44 +0530

    Initial Commit. Setup the overall file structure

---
+++ Backend/src/main/java/com/freakcap/bookExchange/BookExchangeApp/BookExchangeAppApplication.java
@@ -0,0 +1,13 @@
+package com.freakcap.bookExchange.BookExchangeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookExchangeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookExchangeAppApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/freakcap/bookExchange/BookExchangeApp/BookExchangeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freakcap.bookExchange.BookExchangeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookExchangeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
